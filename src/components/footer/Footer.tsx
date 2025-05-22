0eb210640eed03d006b0d3fc74f082db7e88800e
Pamela <pamsilva.trabalho@hotmail.com>
2025-05-22 15:13:50 -0300

    chore: Consuming the personal blog backend API and adjusting the registration page to register new users

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -5,17 +5,23 @@ function Footer() {
 
     return (
         <>
-            <div className="flex justify-center bg-indigo-900 text-white">
+            <div className="flex justify-center bg-pink-500 text-black">
                 <div className="container flex flex-col items-center py-4">
                     <p className='text-xl font-bold'>
                         Blog Pessoal Generation | Copyright: {data}
                     </p>
                     <p className='text-lg'>Acesse nossas redes sociais</p>
                     <div className='flex gap-2'>
+                    <a href="https://www.linkedin.com/in/pamelaoliveira14/" target="_blank">
                         <LinkedinLogoIcon size={48} weight='bold' />
+                    </a>
+                    <a href="https://github.com/heypamela" target="_blank">
                         <GithubLogoIcon size={48} weight='bold' />
+                    </a>
+                    <a href="https://www.instagram.com/generationbrasil/p/DHJ98FCvJdo/" target="_blank">
                         <InstagramLogoIcon size={48} weight='bold' />
-                    </div>
+                    </a>
+                </div>
                 </div>
             </div>
         </>
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,8 +1,8 @@
 function Home() {
     return (
         <>
-            <div className="bg-indigo-900 flex justify-center">
-                <div className='container grid grid-cols-2 text-white'>
+            <div className="bg-pink-500 flex justify-center">
+                <div className='container grid grid-cols-2 text-black'>
                     <div className="flex flex-col gap-4 items-center justify-center py-4">
                         <h2 className='text-5xl font-bold'>
                             Seja Bem Vinde!
@@ -12,7 +12,7 @@ function Home() {
                         </p>
 
                         <div className="flex justify-around gap-4">
-                            <div className='rounded text-white border-white border-solid border-2 py-2 px-4'>
+                            <div className='rounded bg-pink-300 text-black border-black border-solid border-2 py-2 px-4'>
                                 Nova Postagem
                             </div>
                         </div>
@@ -20,7 +20,7 @@ function Home() {
 
                     <div className="flex justify-center ">
                         <img
-                            src="https://i.imgur.com/fyfri1v.png"
+                            src="./src/assets/developer-pink.png"
                             alt="Imagem Página Home"
                             className='w-2/3'
                         />
